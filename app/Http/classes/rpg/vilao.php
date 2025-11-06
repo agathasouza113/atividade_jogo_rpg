ee7b65980bff62d7fdf8b60ee5386c88b2e18cc7
agathasouza113 <agathasouzasenai113@gmail.com>
2025-11-06 14:54:54 -0300

    ComeçoDaPaginaInicial

--- app/Http/classes/rpg/vilao.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace app\Http\classes\rpg;
-use app\Http\classes\rpg\Jogador;
-
-
-
-
-?>
---
+++ resources/views/tela_inicial_jogo.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=], initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>A lenda de Orion</title>
+    <link rel="stylesheet" href="{{ asset('css/tela_inicial.css') }}">
+</head>
+
+<body>
+    <h1 class="NomeGame">A LENDA DE ORION</h1>
+
+    <p>Aqui você vai descobrir novas aventuras e viver uma experiência incrível</p>
+</body>
+</html>
